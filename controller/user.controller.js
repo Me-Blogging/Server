c97b387d119481f0f580e9d8fb4cd3b240a0e16b
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-11-03 15:58:50 +0300

    cmt

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,18 +1,26 @@
 const User = require('../model/user.model')
 const bcrypt = require('bcrypt')
+const saltRounds = 10
 const jwt = require('jsonwebtoken')
 
 exports.SignUp = async (req, res) => {
   const { username, email, password } = req.body
   try {
-    const existingUser = await User.findOne({ email })
+    const user = await User.findOne({ email })
     if (!username || !email || !password) {
       return res.send('All fields required')
     }
-    if (existingUser) {
+    if (user) {
       return res.status(400).send('User already exists')
     }
 
+    const hashedPassword = bcrypt.hash(password, saltRounds)
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword
+    })
+    await newUser.save()
   } catch (error) {
     throw error
   }
@@ -26,8 +34,13 @@ exports.LogIn = async (req, res) => {
     })
     if (!user) return res.status(401).send("Invalid credentials")
 
-    const isPasswordValid = await bcrypt.compare(password, user.password)
+    const isPasswordValid = await bcrypt.compare(password, user.password)    // Bcrypt
     if(!isPasswordValid) return res.status(401).send("Invalid credentials")
+
+    // Token 
+    const payload = {userid: user._id, username: user.username, email: user.email}
+    const token = jwt.sign(payload, process.env.TOKEN_SECRET, {expiresIn: "24h"})
+    res.json({token})
   } catch (error) {
     throw error
   }
